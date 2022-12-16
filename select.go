7ca0394d6513f0d48a96fe7bc286b35f99a18e30
tang-zhenhao <1098985413@qq.com>
2022-12-16 09:48:50 +0800

    SELECT:WHERE语句、Expression抽象

--- select.go
+++ select.go
@@ -2,16 +2,22 @@ package Go_ORM
 
 import (
 	"context"
+	"fmt"
 	"reflect"
 	"strings"
 )
 
 type Selector[T any] struct {
 	table string
+	where []Predicate
+	sb    *strings.Builder
+	args  []any
 }
 
 func (s *Selector[T]) Build() (*Query, error) {
-	var sb strings.Builder
+	// 是指针的话要先初始化好
+	s.sb = &strings.Builder{}
+	sb := s.sb
 	sb.WriteString("SELECT * FROM ")
 	// 我怎么把表名拿到 --> 反射
 	// 如果用户指定了表名, 我们就用表名
@@ -26,14 +32,85 @@ func (s *Selector[T]) Build() (*Query, error) {
 	} else {
 		sb.WriteString(s.table)
 	}
+
+	if len(s.where) > 0 {
+		sb.WriteString(" WHERE ")
+		p := s.where[0]
+		for i := 1; i < len(s.where); i++ {
+			p = p.And(s.where[i])
+		}
+
+		// p 本身就是一个Expression
+		if err := s.buildExpression(p); err != nil {
+			return nil, err
+		}
+	}
+
 	sb.WriteByte(';')
 	return &Query{
-		SQL: sb.String(),
+		SQL:  sb.String(),
+		Args: s.args,
 	}, nil
+}
+
+func (s *Selector[T]) buildExpression(expr Expression) error {
+	switch exp := expr.(type) {
+	case nil:
+	case Predicate:
+		// 在这里处理 p
+		// p.left 构建好
+		// p.op 构建好
+		// p.right 构建好
+		_, ok := exp.left.(Predicate)
+		if ok {
+			s.sb.WriteByte('(')
+		}
+		if err := s.buildExpression(exp.left); err != nil {
+			return err
+		}
+		if ok {
+			s.sb.WriteByte(')')
+		}
+		s.sb.WriteByte(' ')
+		s.sb.WriteString(exp.op.String())
+		s.sb.WriteByte(' ')
+		_, ok = exp.right.(Predicate)
+		if ok {
+			s.sb.WriteByte('(')
+		}
+		if err := s.buildExpression(exp.right); err != nil {
+			return err
+		}
+		if ok {
+			s.sb.WriteByte(')')
+		}
+	case Column:
+		s.sb.WriteByte('`')
+		s.sb.WriteString(exp.name)
+		s.sb.WriteByte('`')
+	case value:
+		s.sb.WriteByte('?')
+		s.addArg(exp.val)
+	default:
+		fmt.Errorf("orm: 不支持的表达式 %v", expr)
+	}
+	return nil
+}
+
+func (s *Selector[T]) addArg(val any) {
+	if s.args == nil {
+		s.args = make([]any, 0, 8)
+	}
+	s.args = append(s.args, val)
 }
 
 func (s *Selector[T]) From(table string) *Selector[T] {
 	s.table = table
+	return s
+}
+
+func (s *Selector[T]) Where(ps ...Predicate) *Selector[T] {
+	s.where = ps
 	return s
 }
 
